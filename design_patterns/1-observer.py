9acf70fbccf19fde0bfda1d6c75f6d1c1de8feac
Pierre1200 <rouvellat.p@icloud.com>
2026-05-21 13:27:26 +0200

    Refactor subscribe method for improved readability

--- design_patterns/1-observer.py
+++ design_patterns/1-observer.py
@@ -11,7 +11,8 @@ class NewsSubject:
     def __init__(self) -> None:
         self._subs: dict[Observer, set[str] | None] = {}
 
-    def subscribe(self, observer: Observer, topics: set[str] | None = None) -> None:
+    def subscribe(self, observer: Observer,
+                  topics: set[str] | None = None) -> None:
         if observer in self._subs:
             return
         self._subs[observer] = topics
